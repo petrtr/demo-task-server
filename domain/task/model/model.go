8d1210e93e9c671c064e0718bc7f6b7e0009a14f
petrtr <petrtr@gmail.com>
2024-02-18 21:50:40 +0300

    sorted output tasks string

--- domain/task/model/model.go
+++ domain/task/model/model.go
@@ -2,8 +2,11 @@ package model
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
+
+const TaskStringFormat = "%d:%d"
 
 type Task struct {
 	Id    int  `json:"id"`
@@ -11,12 +14,16 @@ type Task struct {
 }
 
 func (t Task) String() string {
-	return fmt.Sprintf("%d:%d", t.Id, t.Count)
+	return fmt.Sprintf(TaskStringFormat, t.Id, t.Count)
 }
 
 type Tasks []Task
 
 func (ts Tasks) String() string {
+
+	sort.Slice(ts, func(i, j int) bool {
+		return ts[i].Id < ts[j].Id
+	})
 
 	lines := make([]string, 0, len(ts))
 	for _, task := range ts {
